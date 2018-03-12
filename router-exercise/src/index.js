43915256ff7a4d3bf683e80c3b0703a85e803215
Laszlo-Lazuer <visualjazz@gmail.com>
2018-03-12 01:13:21 -0500

    Router 4 example.

--- router-exercise/src/index.js
+++ router-exercise/src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <BrowserRouter>
+  <div>
+    <Route path='/' component={App} />
+    <Route path='/home' render={() => <h1>Hello</h1>} />
+  </div>
+  </BrowserRouter>,
+  document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept()
+}
